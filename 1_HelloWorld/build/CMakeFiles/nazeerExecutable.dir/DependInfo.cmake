8292fd5651d7552d2509496e288d54f27e274f8e
Mohammad Asif <asif.punchline@gmail.com>
2023-09-10 23:38:28 +0530

    starting with hello world examples

---
+++ 1_HelloWorld/build/CMakeFiles/nazeerExecutable.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/syntiant_0005/asif_syntiant/learning/nazeer_cmake/1_HelloWorld/main.cpp" "CMakeFiles/nazeerExecutable.dir/main.cpp.o" "gcc" "CMakeFiles/nazeerExecutable.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 1_HelloWorld/build/CMakeFiles/nazeerExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nazeerExecutable.dir/main.cpp.o"
+  "CMakeFiles/nazeerExecutable.dir/main.cpp.o.d"
+  "nazeerExecutable"
+  "nazeerExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nazeerExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
